38d8eff8fbed9cdaa1145cf2d8b96cd8adef6d6c
Sito8943 <sito8943@gmail.com>
2025-11-11 15:08:45 +0100

    Refactor using WelcomeController with 'use' import

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
-
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', \App\Http\Controllers\WelcomeController::class . 'index');
+Route::get('/', [WelcomeController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('userzone.dashboard');
