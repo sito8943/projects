436b4c7b2099bed98a4f579a85b923986cdd49ed
Sito <40501794+sito8943@users.noreply.github.com>
2025-11-13 16:41:02 +0100

    FEAUTRE: Pretty access to CRUD actions

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -2,11 +2,26 @@
     <ul class="grid grid-cols-3 grid-rows-1 gap-4">
         @foreach ($tags as $tag)
             <li class="h-full">
-                <a href="/tags/{{ $tag->id }}" class="bg-gray-200 hover:bg-red-400 hover:text-white transition rounded-lg h-full p-4 flex flex-col gap-2">
-                    <h3 class="font-bold text-xl">
-                        {{ $tag->name }}
-                    </h3>
-                </a>
+                <div class="bg-gray-200 transition rounded-lg h-full p-4 flex items-center justify-between gap-2">
+                    <a href="/tags/{{ $tag->id }}"
+                        class="">
+                        <h3 class="font-bold text-xl hover:text-red-400">
+                            {{ $tag->name }}
+                        </h3>
+                    </a>
+                    <ul class="flex gap-4 items-start justify-end">
+                        <a href="/tags/{{ $tag->id }}/edit" class="hover:text-red-400">
+                            Edit
+                        </a>
+                        <form method="POST" action="/tags/{{ $tag->id }}/destroy">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="hover:text-red-400 cursor-pointer">
+                                Delete
+                            </button>
+                        </form>
+                    </ul>
+                </div>
             </li>
         @endforeach
     </ul>
