5798eb7da99e19623f858210b74b3c0b3578014f
Sito <40501794+sito8943@users.noreply.github.com>
2025-11-14 12:55:55 +0100

    FEATURE: add back button

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -10,8 +10,8 @@
 
 <body class="flex flex-col min-h-screen">
     <header
-        class="bg-red-400/70 backdrop-blur-sm rounded-3xl px-4 py-2 flex items-center justify-between mx-9 my-4 sticky top-2">
-        <h1 class="text-xl">
+        class="bg-red-400/70 backdrop-blur-sm rounded-3xl m-auto w-full max-w-[960px] px-4 py-2 flex items-center justify-between my-4 sticky top-2">
+        <h1 class="text-xl poppins">
             <a href="/" class="text-white">
                 Proctique
             </a>
@@ -38,10 +38,18 @@
             </ul>
         </nav>
     </header>
-    <main class="flex-1 h-full px-10 pb-4">
-        <h2 class="text-3xl font-bold mb-4">
-            {{ $title }}
-        </h2>
+    <main class="flex-1 h-full m-auto w-full max-w-[960px]  pb-4">
+        <div class="flex items-center gap-2 mb-4">
+            @if ($title !== 'Proctique')
+                <button type="button" onclick="history.go(-1)"
+                    class="rounded-full hover:bg-red-400/40 p-2 transition hover:text-white">
+                    <x-fas-chevron-left class="w-4 h-4" />
+                </button>
+            @endif
+            <h2 class="text-3xl font-bold">
+                {{ $title }}
+            </h2>
+        </div>
 
         <section>
             {{ $slot }}
